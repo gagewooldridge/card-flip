8158db63df0f75f5f0b6bd6384cdd2bafcfa08bf
gagewooldridge@gmail.com <gagewooldridge@gmail.com>
2016-05-16 17:04:07 -0500

    Card flip lab
    Program that takes five cards and allows the user to flip the card from
    face up to face down and displays the name of the card.

---
+++ 3333_WooldridgeG_Lab02/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace _3333_WooldridgeG_Lab02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cardOneBack = new System.Windows.Forms.PictureBox();
+            this.cardTwoBack = new System.Windows.Forms.PictureBox();
+            this.cardThreeBack = new System.Windows.Forms.PictureBox();
+            this.cardFourBack = new System.Windows.Forms.PictureBox();
+            this.cardFiveBack = new System.Windows.Forms.PictureBox();
+            this.cardOneFront = new System.Windows.Forms.PictureBox();
+            this.cardTwoFront = new System.Windows.Forms.PictureBox();
+            this.cardThreeFront = new System.Windows.Forms.PictureBox();
+            this.cardFourFront = new System.Windows.Forms.PictureBox();
+            this.cardFiveFront = new System.Windows.Forms.PictureBox();
+            this.cardNameLabel = new System.Windows.Forms.Label();
+            this.userPromptLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cardOneBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardTwoBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardThreeBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardFourBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardFiveBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardOneFront)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardTwoFront)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardThreeFront)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardFourFront)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardFiveFront)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cardOneBack
+            // 
+            this.cardOneBack.Image = ((System.Drawing.Image)(resources.GetObject("cardOneBack.Image")));
+            this.cardOneBack.ImageLocation = "";
+            this.cardOneBack.Location = new System.Drawing.Point(12, 167);
+            this.cardOneBack.Name = "cardOneBack";
+            this.cardOneBack.Size = new System.Drawing.Size(125, 175);
+            this.cardOneBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardOneBack.TabIndex = 0;
+            this.cardOneBack.TabStop = false;
+            this.cardOneBack.Click += new System.EventHandler(this.cardOneBack_Click);
+            // 
+            // cardTwoBack
+            // 
+            this.cardTwoBack.Image = ((System.Drawing.Image)(resources.GetObject("cardTwoBack.Image")));
+            this.cardTwoBack.Location = new System.Drawing.Point(167, 167);
+            this.cardTwoBack.Name = "cardTwoBack";
+            this.cardTwoBack.Size = new System.Drawing.Size(125, 175);
+            this.cardTwoBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardTwoBack.TabIndex = 1;
+            this.cardTwoBack.TabStop = false;
+            this.cardTwoBack.Click += new System.EventHandler(this.cardTwoBack_Click);
+            // 
+            // cardThreeBack
+            // 
+            this.cardThreeBack.Image = ((System.Drawing.Image)(resources.GetObject("cardThreeBack.Image")));
+            this.cardThreeBack.Location = new System.Drawing.Point(328, 167);
+            this.cardThreeBack.Name = "cardThreeBack";
+            this.cardThreeBack.Size = new System.Drawing.Size(125, 175);
+            this.cardThreeBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardThreeBack.TabIndex = 2;
+            this.cardThreeBack.TabStop = false;
+            this.cardThreeBack.Click += new System.EventHandler(this.cardThreeBack_Click);
+            // 
+            // cardFourBack
+            // 
+            this.cardFourBack.Image = ((System.Drawing.Image)(resources.GetObject("cardFourBack.Image")));
+            this.cardFourBack.Location = new System.Drawing.Point(487, 167);
+            this.cardFourBack.Name = "cardFourBack";
+            this.cardFourBack.Size = new System.Drawing.Size(125, 175);
+            this.cardFourBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardFourBack.TabIndex = 3;
+            this.cardFourBack.TabStop = false;
+            this.cardFourBack.Click += new System.EventHandler(this.cardFourBack_Click);
+            // 
+            // cardFiveBack
+            // 
+            this.cardFiveBack.Image = ((System.Drawing.Image)(resources.GetObject("cardFiveBack.Image")));
+            this.cardFiveBack.Location = new System.Drawing.Point(645, 167);
+            this.cardFiveBack.Name = "cardFiveBack";
+            this.cardFiveBack.Size = new System.Drawing.Size(125, 175);
+            this.cardFiveBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardFiveBack.TabIndex = 4;
+            this.cardFiveBack.TabStop = false;
+            this.cardFiveBack.Click += new System.EventHandler(this.cardFiveBack_Click);
+            // 
+            // cardOneFront
+            // 
+            this.cardOneFront.Image = ((System.Drawing.Image)(resources.GetObject("cardOneFront.Image")));
+            this.cardOneFront.Location = new System.Drawing.Point(12, 167);
+            this.cardOneFront.Name = "cardOneFront";
+            this.cardOneFront.Size = new System.Drawing.Size(125, 175);
+            this.cardOneFront.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardOneFront.TabIndex = 5;
+            this.cardOneFront.TabStop = false;
+            this.cardOneFront.Click += new System.EventHandler(this.cardOneFront_Click);
+            // 
+            // cardTwoFront
+            // 
+            this.cardTwoFront.Image = ((System.Drawing.Image)(resources.GetObject("cardTwoFront.Image")));
+            this.cardTwoFront.Location = new System.Drawing.Point(167, 167);
+            this.cardTwoFront.Name = "cardTwoFront";
+            this.cardTwoFront.Size = new System.Drawing.Size(125, 175);
+            this.cardTwoFront.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardTwoFront.TabIndex = 6;
+            this.cardTwoFront.TabStop = false;
+            this.cardTwoFront.Click += new System.EventHandler(this.cardTwoFront_Click);
+            // 
+            // cardThreeFront
+            // 
+            this.cardThreeFront.Image = ((System.Drawing.Image)(resources.GetObject("cardThreeFront.Image")));
+            this.cardThreeFront.Location = new System.Drawing.Point(328, 167);
+            this.cardThreeFront.Name = "cardThreeFront";
+            this.cardThreeFront.Size = new System.Drawing.Size(125, 175);
+            this.cardThreeFront.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardThreeFront.TabIndex = 7;
+            this.cardThreeFront.TabStop = false;
+            this.cardThreeFront.Click += new System.EventHandler(this.cardThreeFront_Click);
+            // 
+            // cardFourFront
+            // 
+            this.cardFourFront.Image = ((System.Drawing.Image)(resources.GetObject("cardFourFront.Image")));
+            this.cardFourFront.Location = new System.Drawing.Point(487, 167);
+            this.cardFourFront.Name = "cardFourFront";
+            this.cardFourFront.Size = new System.Drawing.Size(125, 175);
+            this.cardFourFront.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardFourFront.TabIndex = 8;
+            this.cardFourFront.TabStop = false;
+            this.cardFourFront.Click += new System.EventHandler(this.cardFourFront_Click);
+            // 
+            // cardFiveFront
+            // 
+            this.cardFiveFront.Image = ((System.Drawing.Image)(resources.GetObject("cardFiveFront.Image")));
+            this.cardFiveFront.Location = new System.Drawing.Point(645, 167);
+            this.cardFiveFront.Name = "cardFiveFront";
+            this.cardFiveFront.Size = new System.Drawing.Size(125, 175);
+            this.cardFiveFront.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardFiveFront.TabIndex = 9;
+            this.cardFiveFront.TabStop = false;
+            this.cardFiveFront.Click += new System.EventHandler(this.cardFiveFront_Click);
+            // 
+            // cardNameLabel
+            // 
+            this.cardNameLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.cardNameLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cardNameLabel.Location = new System.Drawing.Point(276, 383);
+            this.cardNameLabel.Name = "cardNameLabel";
+            this.cardNameLabel.Size = new System.Drawing.Size(225, 40);
+            this.cardNameLabel.TabIndex = 10;
+            this.cardNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // userPromptLabel
+            // 
+            this.userPromptLabel.AutoSize = true;
+            this.userPromptLabel.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userPromptLabel.Location = new System.Drawing.Point(248, 57);
+            this.userPromptLabel.Name = "userPromptLabel";
+            this.userPromptLabel.Size = new System.Drawing.Size(296, 39);
+            this.userPromptLabel.TabIndex = 11;
+            this.userPromptLabel.Text = "Please choose a card";
+            this.userPromptLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.Controls.Add(this.userPromptLabel);
+            this.Controls.Add(this.cardNameLabel);
+            this.Controls.Add(this.cardOneBack);
+            this.Controls.Add(this.cardOneFront);
+            this.Controls.Add(this.cardTwoBack);
+            this.Controls.Add(this.cardTwoFront);
+            this.Controls.Add(this.cardThreeBack);
+            this.Controls.Add(this.cardThreeFront);
+            this.Controls.Add(this.cardFourBack);
+            this.Controls.Add(this.cardFourFront);
+            this.Controls.Add(this.cardFiveBack);
+            this.Controls.Add(this.cardFiveFront);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cardOneBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardTwoBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardThreeBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardFourBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardFiveBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardOneFront)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardTwoFront)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardThreeFront)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardFourFront)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardFiveFront)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cardOneBack;
+        private System.Windows.Forms.PictureBox cardTwoBack;
+        private System.Windows.Forms.PictureBox cardThreeBack;
+        private System.Windows.Forms.PictureBox cardFourBack;
+        private System.Windows.Forms.PictureBox cardFiveBack;
+        private System.Windows.Forms.PictureBox cardOneFront;
+        private System.Windows.Forms.PictureBox cardTwoFront;
+        private System.Windows.Forms.PictureBox cardThreeFront;
+        private System.Windows.Forms.PictureBox cardFourFront;
+        private System.Windows.Forms.PictureBox cardFiveFront;
+        private System.Windows.Forms.Label cardNameLabel;
+        private System.Windows.Forms.Label userPromptLabel;
+
+    }
+}
+
---
+++ 3333_WooldridgeG_Lab02/Form1.cs
@@ -0,0 +1,146 @@
+﻿// This program will take five cards, which when
+// clicked will show the face of the card and display
+// the name of the card in a label
+
+// Written by Gage Wooldridge
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3333_WooldridgeG_Lab02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cardOneBack_Click(object sender, EventArgs e)
+        {
+            // Shows face of card one
+
+            cardOneFront.BringToFront();
+            cardTwoFront.SendToBack();
+            cardThreeFront.SendToBack();
+            cardFourFront.SendToBack();
+            cardFiveFront.SendToBack();
+
+            // Display card name in a label
+
+            cardNameLabel.Text = "Four of Clubs";
+        }
+
+        private void cardTwoBack_Click(object sender, EventArgs e)
+        {
+            // Shows face of card two
+
+            cardTwoFront.BringToFront();
+            cardOneFront.SendToBack();
+            cardThreeFront.SendToBack();
+            cardFourFront.SendToBack();
+            cardFiveFront.SendToBack();
+
+            // Display card name in a label
+
+            cardNameLabel.Text = "Jack of Clubs";
+        }
+
+        private void cardThreeBack_Click(object sender, EventArgs e)
+        {
+            // Shows face of card three
+
+            cardThreeFront.BringToFront();
+            cardTwoFront.SendToBack();
+            cardOneFront.SendToBack();
+            cardFourFront.SendToBack();
+            cardFiveFront.SendToBack();
+
+            // Display card name in a label
+
+            cardNameLabel.Text = "Nine of Diamonds";
+        }
+
+        private void cardFourBack_Click(object sender, EventArgs e)
+        {
+            // Shows face of card four
+
+            cardFourFront.BringToFront();
+            cardTwoFront.SendToBack();
+            cardThreeFront.SendToBack();
+            cardOneFront.SendToBack();
+            cardFiveFront.SendToBack();
+
+            // Display card name in a label
+
+            cardNameLabel.Text = "Ace of Hearts";
+        }
+
+        private void cardFiveBack_Click(object sender, EventArgs e)
+        {
+            // Shows face of card five
+
+            cardFiveFront.BringToFront();
+            cardTwoFront.SendToBack();
+            cardThreeFront.SendToBack();
+            cardFourFront.SendToBack();
+            cardOneFront.SendToBack();
+
+            // Doisplay card name in a label
+
+            cardNameLabel.Text = "Ten of Diamonds";
+        }
+
+        private void cardOneFront_Click(object sender, EventArgs e)
+        {
+            // Shows back of card one
+            // Clears label
+
+            cardOneBack.BringToFront();
+            cardNameLabel.Text = " ";
+        }
+
+        private void cardTwoFront_Click(object sender, EventArgs e)
+        {
+            // Shows back of card two
+            // Clears label
+
+            cardTwoBack.BringToFront();
+            cardNameLabel.Text = " ";
+        }
+
+        private void cardThreeFront_Click(object sender, EventArgs e)
+        {
+            // Shows back of card three
+            // Clears label
+
+            cardThreeBack.BringToFront();
+            cardNameLabel.Text = " ";
+        }
+
+        private void cardFourFront_Click(object sender, EventArgs e)
+        {
+            // Shows back of card Four
+            // Clears label
+
+            cardFourBack.BringToFront();
+            cardNameLabel.Text = " ";
+        }
+
+        private void cardFiveFront_Click(object sender, EventArgs e)
+        {
+            // Shows back of card Five
+            // Clears label
+
+            cardFiveBack.BringToFront();
+            cardNameLabel.Text = " ";
+        }
+    }
+}
